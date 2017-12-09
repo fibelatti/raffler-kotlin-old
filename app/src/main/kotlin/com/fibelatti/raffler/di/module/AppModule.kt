019791e391ea2614862bf2681896d599b3af71dc
Filipe Belatti <fibelatti@gmail.com>
2017-12-09 15:27:19 +0000

    Including database in DI

--- app/src/main/kotlin/com/fibelatti/raffler/di/module/AppModule.kt
+++ app/src/main/kotlin/com/fibelatti/raffler/di/module/AppModule.kt
@@ -1,11 +1,13 @@
 package com.fibelatti.raffler.di.module
 
+import android.arch.persistence.room.Room
 import android.content.Context
 import android.content.SharedPreferences
 import android.preference.PreferenceManager
 import com.fibelatti.raffler.App
 import com.fibelatti.raffler.core.UserPreferencesContract
 import com.fibelatti.raffler.core.UserPreferencesManager
+import com.fibelatti.raffler.data.localdatastorage.AppDatabase
 import com.fibelatti.raffler.di.qualifier.AppQualifier
 import com.fibelatti.raffler.di.scope.AppScope
 import dagger.Module
@@ -30,4 +32,14 @@ class AppModule(private var app: App) {
     @Provides
     @AppScope
     fun provideUserPreferences(sharedPreferences: SharedPreferences): UserPreferencesContract = UserPreferencesManager(sharedPreferences)
+
+    @Provides
+    @AppScope
+    fun providesDatabase(@AppQualifier context: Context): AppDatabase {
+        return Room.databaseBuilder(context,
+                AppDatabase::class.java, AppDatabase.DATABASE_NAME)
+                .addMigrations(AppDatabase.MigrationTo4, AppDatabase.MigrationTo5)
+                .addCallback(AppDatabase.RoomCallback)
+                .build()
+    }
 }
