29c439baf254d72db0a1de0893ad032773a7ef38
Filipe Belatti <fibelatti@gmail.com>
2017-12-10 09:13:42 +0000

    Adjusting presentation models

--- app/src/main/kotlin/com/fibelatti/raffler/presentation/models/Group.kt
+++ app/src/main/kotlin/com/fibelatti/raffler/presentation/models/Group.kt
@@ -1,7 +1,11 @@
 package com.fibelatti.raffler.presentation.models
 
-data class Group(val id: Long, val name: String, val items: List<GroupItem>) {
-    val groupItemNames: List<String>?
+import android.os.Parcel
+import android.os.Parcelable
+import com.fibelatti.raffler.core.extensions.createParcel
+
+data class Group(val id: Long, var name: String, var items: List<GroupItem>) : Parcelable {
+    val groupItemNames: List<String>
         get() = items.map { item -> item.name }
 
 
@@ -13,6 +17,25 @@ data class Group(val id: Long, val name: String, val items: List<GroupItem>) {
 
     val selectedItems: List<GroupItem>
         get() = items.filter { item -> item.isIncluded }
+
+    constructor(parcel: Parcel) : this(
+            parcel.readValue(Long::class.java.classLoader) as Long,
+            parcel.readString(),
+            mutableListOf<GroupItem>().apply {
+                parcel.readList(this, GroupItem::class.java.classLoader)
+            })
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeValue(id)
+        parcel.writeString(name)
+        parcel.writeList(items)
+    }
+
+    override fun describeContents() = 0
+
+    companion object {
+        val CREATOR = createParcel { Group(it) }
+    }
 }
 
 
