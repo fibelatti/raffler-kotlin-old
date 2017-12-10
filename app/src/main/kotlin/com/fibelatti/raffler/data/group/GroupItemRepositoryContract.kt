c3cd6713cb1c5c8dc7a4337b7d2c8115a1e3fe0e
Filipe Belatti <fibelatti@gmail.com>
2017-12-10 09:10:01 +0000

    Fixing repositories

--- app/src/main/kotlin/com/fibelatti/raffler/data/group/GroupItemRepositoryContract.kt
+++ app/src/main/kotlin/com/fibelatti/raffler/data/group/GroupItemRepositoryContract.kt
@@ -1,6 +1,8 @@
 package com.fibelatti.raffler.data.group
 
 import android.arch.persistence.room.Dao
+import android.arch.persistence.room.Insert
+import android.arch.persistence.room.OnConflictStrategy
 import android.arch.persistence.room.Query
 import io.reactivex.Single
 
@@ -10,6 +12,12 @@ interface GroupItemRepositoryContract {
             " where " + GroupItem.COLUMN_GROUP_ID + " = :groupId")
     fun getAllGroupItemsByGroupId(groupId: Long): Single<List<GroupItem>>
 
-    @Query("delete from " + GroupItem.TABLE_NAME + " where " + GroupItem.COLUMN_ID + " in(:groupItemId)")
-    fun deleteGroupItemsById(groupItemIds: List<Long>): Single<Boolean>
+    @Query("delete from " + GroupItem.TABLE_NAME + " where " + GroupItem.COLUMN_GROUP_ID + " = :groupId")
+    fun deleteGroupItemsByGroupId(groupId: Long)
+
+    @Query("delete from " + GroupItem.TABLE_NAME + " where " + GroupItem.COLUMN_ID + " in(:groupItemIds)")
+    fun deleteGroupItemsById(groupItemIds: List<Long>)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun saveGroupItems(vararg groupItems: GroupItem)
 }
--- app/src/main/kotlin/com/fibelatti/raffler/data/group/GroupRepositoryContract.kt
+++ app/src/main/kotlin/com/fibelatti/raffler/data/group/GroupRepositoryContract.kt
@@ -4,20 +4,23 @@ import android.arch.persistence.room.Dao
 import android.arch.persistence.room.Insert
 import android.arch.persistence.room.OnConflictStrategy.REPLACE
 import android.arch.persistence.room.Query
+import android.arch.persistence.room.Transaction
 import io.reactivex.Single
 
 @Dao
 interface GroupRepositoryContract {
+    @Transaction
     @Query("select * from " + Group.TABLE_NAME +
             " where " + Group.COLUMN_ID + " = :groupId ")
-    fun getGroupById(groupId: Long): Single<Group>
+    fun getGroupById(groupId: Long): Single<GroupWithItems>
 
+    @Transaction
     @Query("select * from " + Group.TABLE_NAME)
-    fun getAllGroups(): Single<List<Group>>
+    fun getAllGroups(): Single<List<GroupWithItems>>
 
     @Insert(onConflict = REPLACE)
-    fun saveGroup(group: Group): Single<Boolean>
+    fun saveGroup(group: Group)
 
     @Query("delete from " + Group.TABLE_NAME + " where " + Group.COLUMN_ID + " = :groupId")
-    fun deleteGroupById(groupId: Long): Single<Boolean>
+    fun deleteGroupById(groupId: Long)
 }
