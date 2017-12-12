0979e7026e48fb9fc8bff7de19a40fb97af1275b
Filipe Belatti <fibelatti@gmail.com>
2017-12-12 11:40:26 +0000

    Creating base database test

---
+++ app/src/androidTest/kotlin/com/fibelatti/raffler/BaseDbTest.kt
@@ -0,0 +1,22 @@
+package com.fibelatti.raffler
+
+import android.arch.persistence.room.Room
+import android.support.test.InstrumentationRegistry
+import com.fibelatti.raffler.data.localdatastorage.AppDatabase
+import org.junit.After
+import org.junit.Before
+
+abstract class BaseDbTest: BaseTest() {
+    protected lateinit var appDatabase: AppDatabase
+
+    @Before
+    fun initDb() {
+        appDatabase = Room.inMemoryDatabaseBuilder(InstrumentationRegistry.getContext(),
+                AppDatabase::class.java).build()
+    }
+
+    @After
+    fun closeDb() {
+        appDatabase.close()
+    }
+}
