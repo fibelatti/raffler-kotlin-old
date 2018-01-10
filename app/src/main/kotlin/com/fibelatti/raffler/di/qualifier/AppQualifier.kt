9fe77d266a7686537e568ba7ebe8316a919b14f3
Filipe Belatti <fibelatti@gmail.com>
2018-01-10 09:41:04 +0000

    Removes Dagger classes

--- app/src/main/kotlin/com/fibelatti/raffler/di/qualifier/AppQualifier.kt
+++
@@ -1,7 +0,0 @@
-package com.fibelatti.raffler.di.qualifier
-
-import javax.inject.Qualifier
-
-@Qualifier
-@Retention(AnnotationRetention.RUNTIME)
-annotation class AppQualifier
--- app/src/main/kotlin/com/fibelatti/raffler/di/scope/PresentationScope.kt
+++
@@ -1,7 +0,0 @@
-package com.fibelatti.raffler.di.scope
-
-import javax.inject.Scope
-
-@Scope
-@Retention(AnnotationRetention.RUNTIME)
-annotation class PresentationScope
