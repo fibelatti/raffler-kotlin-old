4761cb2cea3a163210ebeb4c0e234f1636b1c92a
Filipe Belatti <fibelatti@gmail.com>
2018-01-10 09:45:02 +0000

    Initialises Koin

--- app/src/main/kotlin/com/fibelatti/raffler/App.kt
+++ app/src/main/kotlin/com/fibelatti/raffler/App.kt
@@ -1,23 +1,12 @@
 package com.fibelatti.raffler
 
 import android.app.Application
-import com.fibelatti.raffler.di.component.AppComponent
-import com.fibelatti.raffler.di.component.DaggerAppComponent
-import com.fibelatti.raffler.di.module.AppModule
+import com.fibelatti.raffler.di.allModules
+import org.koin.android.ext.android.startKoin
 
 class App : Application() {
-    companion object {
-        lateinit var appComponent: AppComponent
-    }
-
     override fun onCreate() {
         super.onCreate()
-        buildComponents()
-    }
-
-    private fun buildComponents() {
-        appComponent = DaggerAppComponent.builder()
-                .appModule(AppModule(this))
-                .build()
+        startKoin(this, allModules)
     }
 }
