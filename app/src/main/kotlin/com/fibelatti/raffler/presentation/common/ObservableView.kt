95a2712fb612d0a21fba160bb7249be14ae26dfb
Filipe Belatti <fibelatti@gmail.com>
2018-01-23 10:43:13 +0000

    Fixes the Subject in ObservableView

--- app/src/main/kotlin/com/fibelatti/raffler/presentation/common/ObservableView.kt
+++ app/src/main/kotlin/com/fibelatti/raffler/presentation/common/ObservableView.kt
@@ -1,10 +1,10 @@
 package com.fibelatti.raffler.presentation.common
 
 import io.reactivex.Observable
-import io.reactivex.subjects.BehaviorSubject
+import io.reactivex.subjects.PublishSubject
 
 class ObservableView<Emits> {
-    private val subject = BehaviorSubject.create<Emits>()
+    private val subject = PublishSubject.create<Emits>()
 
     fun getObservable(): Observable<Emits> = subject
 
